2436cb5bd4a64cf6acc391b170977f906a0fcaeb
bluerat1976 <osoblivaia76@mail.ru>
2019-04-14 15:07:14 +0300

    the same problem

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-     
+    var currentCount = (parseInt(prompt('input number')));
+    function makeCounter() {
+      // возвращаемся к функции
+      function counter() {
+        return currentCount++;
+      }
+      return counter;
+    }
+    var counter = makeCounter();
+    var button = document.getElementById('button');
+    button.addEventListener('click', func);
+    function func() {
+      var div = document.getElementById('result');
+      div.innerHTML = counter();
+    }
